4080bfc2e84c2f71f8f2944f5006831849a3585b
Sendilkumar Murugaiah <sendilkumarmv@hotmail.com>
2019-11-03 23:05:21 +0530

    Some more data changes.

---
+++ Docs.Web/Docs.Data/Entities/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Docs.Data.Entities
+{
+    public class Document : BaseEntity
+    {
+        public Document()
+        {
+
+        }
+        public string Name { get; set; }
+        public DocumentType DocumentType { get; set; }
+        public string Created { get; set; }
+        public string CreatedBy { get; set; }
+        public string Modified { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ Docs.Web/Docs.Data/GeneralTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Docs.Data
+{
+    public enum DocumentType
+    {
+        MsWord = 0,
+        MsExcel,
+        MsPowerPoint,
+        AdobePdf,
+        Others
+    }
+}
