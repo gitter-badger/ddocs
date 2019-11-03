859a28cd2726367e048419b2b284495bf6cace7f
Sendilkumar Murugaiah <sendilkumarmv@hotmail.com>
2019-11-03 21:52:11 +0530

    Data project added.

---
+++ Docs.Web/Docs.Web/Controllers/DocumentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Docs.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        public DocumentsController()
+        {
+
+        }
+    }
+}
